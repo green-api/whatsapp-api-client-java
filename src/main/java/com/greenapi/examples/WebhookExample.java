af3b35fb6b6cbecfd606465243b6632034208175
dany47788 <65661948+dany47788@users.noreply.github.com>
2023-09-22 21:05:35 -0300

    remove main class

--- src/main/java/com/greenapi/examples/WebhookExample.java
+++ src/main/java/com/greenapi/examples/WebhookExample.java
@@ -4,9 +4,8 @@ import com.greenapi.pkg.api.webhook.WebhookConsumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class WebhookExample {
-    public static void main(String[] args) {
+    public void webhookExample(String[] args) {
         var context = SpringApplication.run(WebhookExample.class, args);
 
         var webhookConsumer = (WebhookConsumer) context.getBean("webhookConsumer");
