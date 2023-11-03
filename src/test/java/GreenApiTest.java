30259643ce84c79c280e6148816b90b304abdca1
dany47788 <65661948+dany47788@users.noreply.github.com>
2023-11-03 16:44:35 -0300

    fix tests

--- src/test/java/GreenApiTest.java
+++ src/test/java/GreenApiTest.java
@@ -1,6 +1,8 @@
 import com.greenapi.client.pkg.api.GreenApi;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
 import org.springframework.test.context.event.annotation.BeforeTestExecution;
 import org.springframework.web.client.RestTemplate;
 
@@ -29,7 +31,7 @@ public class GreenApiTest {
     protected String fileUrl;
     protected GreenApi greenApi;
 
-    @BeforeTestExecution
+    @BeforeEach
     public void init() {
         greenApi = new GreenApi(new RestTemplate(), hostMedia, host, instanceId, instanceToken);
     }
